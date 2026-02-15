e7fc9232766dc4c22bb7b7dff418ab4ab346c5ab
mayak1337 <53903529+mayak1337@users.noreply.github.com>
2026-02-15 16:37:44 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -307,3 +307,17 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+from flask import Flask
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is alive!"
+
+def run():
+    app.run(host="0.0.0.0", port=8080)
+
+Thread(target=run).start()
